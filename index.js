ad67a082e79182addbc42111d5ea954cfb285b90
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2021-07-28 08:46:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello world this is node app practices with Jenkins successfully 24 times')
+  response.send('Hello world this is node app practices with Jenkins successfully 25 times')
 }) 
 
 app.listen(app.get('port'), function() {
